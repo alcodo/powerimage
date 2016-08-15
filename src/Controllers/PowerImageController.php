3a6e85dd5e25a08e978de6fb7e9d81d4127e25a8
approached <alexejkloos@gmail.com>
2016-08-16 00:06:59 +0200

    * typo

--- src/Controllers/PowerImageController.php
+++ src/Controllers/PowerImageController.php
@@ -10,68 +10,63 @@ use League\Flysystem\Filesystem;
 
 class PowerImageController extends Controller
 {
-    public function show($path, \League\Glide\Server $server)
+    public function show($filepath, \League\Glide\Server $server)
     {
         $params = Input::query();
 
         // original image output
         if (empty($params)) {
-            return $this->showOriginalImage($path, $server);
+            return $this->showOriginalImage($filepath, $server);
         }
 
-        // resized which not exists must be optimized
-        if ($server->cacheFileExists($path, $params) === false) {
-
-            // generate image
-            try {
-                $cacheFile = $server->makeImage($path, $params);
-            } catch (\Exception $e) {
-                abort(404);
-            }
-            $absoluteFilepath = $this->getAbsoulteFilepath($cacheFile, $server);
-
-            // optimize image
-            $optimizer = app('Approached\LaravelImageOptimizer\ImageOptimizer');
-            $optimizer->optimizeImage($absoluteFilepath, pathinfo($path, PATHINFO_EXTENSION));
+        if ($server->cacheFileExists($filepath, $params) === false) {
+            // resize file must created
+            $cacheFile = $this->generateImage($filepath, $params, $server);
+        } else {
+            // resize file exists
+            $cacheFile = $server->makeImage($filepath, $params);
         }
 
-        // resize image exists
-        if (! isset($cacheFile)) {
-            $cacheFile = $server->makeImage($path, $params);
-        }
-
+        // response
         $file = $server->getCache();
-
         $headers = [];
         $headers['Content-Type'] = $file->getMimetype($cacheFile);
         $headers['Content-Length'] = $file->getSize($cacheFile);
         $headers['Cache-Control'] = 'max-age=108000, public';
-        $headers['Expires'] = date_create('+30 days')->format('D, d M Y H:i:s').' GMT';
+        $headers['Expires'] = date_create('+30 days')->format('D, d M Y H:i:s') . ' GMT';
         $headers['PowerImage'] = 'Compressed';
 
         return Response::make($file->read($cacheFile), 200, $headers);
     }
 
-    protected function showOriginalImage($path, \League\Glide\Server $server)
+    protected function showOriginalImage($filepath, \League\Glide\Server $server)
     {
-        $path = $server->getSourcePath($path);
+        $filepath = $server->getSourcePath($filepath);
         $file = $server->getCache();
 
-        if ($file->has($path) === false) {
+        if ($file->has($filepath) === false) {
             abort(404);
         }
 
+        // response
         $headers = [];
-        $headers['Content-Type'] = $file->getMimetype($path);
-        $headers['Content-Length'] = $file->getSize($path);
+        $headers['Content-Type'] = $file->getMimetype($filepath);
+        $headers['Content-Length'] = $file->getSize($filepath);
         $headers['Cache-Control'] = 'max-age=324000, public';
-        $headers['Expires'] = date_create('+30 days')->format('D, d M Y H:i:s').' GMT';
+        $headers['Expires'] = date_create('+30 days')->format('D, d M Y H:i:s') . ' GMT';
         $headers['PowerImage'] = 'Original';
 
-        return Response::make($file->read($path), 200, $headers);
+        return Response::make($file->read($filepath), 200, $headers);
     }
 
-    protected function getAbsoulteFilepath($path, \League\Glide\Server $server)
+    /**
+     * Returns a absoulte filepath from path
+     *
+     * @param $filepath
+     * @param \League\Glide\Server $server
+     * @return string
+     */
+    protected function getAbsoulteFilepath($filepath, \League\Glide\Server $server)
     {
         /** @var Filesystem $filesystem */
         $filesystem = $server->getSource();
@@ -79,6 +74,31 @@ class PowerImageController extends Controller
         /** @var Local $local */
         $local = $filesystem->getAdapter();
 
-        return $local->getPathPrefix().$path;
+        return $local->getPathPrefix() . $filepath;
+    }
+
+    /**
+     * Image will be resize and optimized
+     *
+     * @param $filepath
+     * @param $params
+     * @param $server
+     * @return string
+     */
+    private function generateImage($filepath, $params, $server)
+    {
+        // resize image
+        try {
+            $cacheFile = $server->makeImage($filepath, $params);
+        } catch (\Exception $e) {
+            abort(404);
+        }
+        $absoluteFilepath = $this->getAbsoulteFilepath($cacheFile, $server);
+
+        // optimize image
+        $optimizer = app('Approached\LaravelImageOptimizer\ImageOptimizer');
+        $optimizer->optimizeImage($absoluteFilepath, pathinfo($filepath, PATHINFO_EXTENSION));
+
+        return $cacheFile;
     }
 }
--- src/routes.php
+++ src/routes.php
@@ -1,8 +1,8 @@
 <?php
 
 Route::group(['namespace' => 'Alcodo\PowerImage\Controllers'], function () {
-    Route::get('/powerimage/{path}', [
+    Route::get('/powerimage/{filepath}', [
         'as' => 'powerimage.show',
         'uses' => 'PowerImageController@show',
-    ])->where('path', '.+');
+    ])->where('filepath', '.+');
 });
